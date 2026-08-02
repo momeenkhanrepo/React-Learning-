b82c370e7f4943f07d2cde9eadd124f0a302bd8c
Momeen Khan <mk8350103@gmail.com>
2026-08-02 14:03:32 +0530

    useEffect Hook

---
+++ 16-useeffect-hooks/src/App.jsx
@@ -0,0 +1,42 @@
+// 
+
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
+
+  let aChanging = ()=>{
+    console.log('a is changing');
+  }
+
+  let bChanging = ()=>{
+    console.log('b is changing');
+  }
+
+  useEffect(()=>{
+    aChanging()
+  },[a])
+
+  useEffect(()=>{
+    bChanging()
+  },[b])
+
+
+  return (
+    <div>
+      <h1>Value of a is {a}</h1>
+      <h2>Value of b is {b}</h2>
+
+      <button onClick={()=>{
+        setA(a+1)
+      }}>Change A</button>
+      <button onClick={()=>{
+        setB(b-1)
+      }}>Change B</button>
+    </div>
+  )
+}
+
+export default App
